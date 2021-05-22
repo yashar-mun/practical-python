c300f3cd932bd2b44705c0c0d34c89c45358ccb0
Yashar Tavakoli <yt3165@mun.ca>
2021-05-22 11:48:34 -0230

    Exercise 1.11

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -10,9 +10,14 @@ months = 0
 extra_payment_start_month = 61
 extra_payment_end_month = 108
 extra_payment = 1000
+overpayment = False
 
-while principal > 0:
-    principal = principal * (1 + rate / 12) - payment
+while True:
+    principal_temp = principal * (1 + rate / 12) - payment
+    if principal_temp < 0:
+        break
+
+    principal = principal_temp
     total_paid = total_paid + payment
     months = months + 1
 
@@ -21,4 +26,9 @@ while principal > 0:
         total_paid = total_paid + extra_payment
     print(months, round(total_paid,2), round(principal, 2))
 
-print('Total paid', round(total_paid,2), 'in', months, 'months')
+if principal_temp < 0:
+    print(months + 1, round(principal,2), 0.00)
+    print('Total paid', round(total_paid + principal,2), 'in', months + 1, 'months')
+
+else:
+    print('Total paid', round(total_paid,2), 'in', months, 'months')
