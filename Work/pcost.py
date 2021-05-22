72f18955d0b5e541f728219172874923ef00ef93
Yashar Tavakoli <yt3165@mun.ca>
2021-05-22 14:47:08 -0230

    Exercise 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,10 +1,12 @@
 # pcost.py
 
+import csv
+
 def portfolio_cost(filename):
     total_cost = 0.0
-    with open(filename) as file:
+    with open(filename) as file: 
+        rows = csv.reader(file)  
         next(file, None) 
-        rows = [line.strip().split(',') for line in file]   
         for row in rows:
             try:             
                 total_cost = total_cost + int(row[1]) * float(row[2])
