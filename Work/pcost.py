c313ac11d0ff5a8f92179b76d3fe6cc39198d03b
Yashar Tavakoli <yt3165@mun.ca>
2021-05-22 14:35:36 -0230

    Exercise 1.31

--- Work/pcost.py
+++ Work/pcost.py
@@ -5,8 +5,11 @@ def portfolio_cost(filename):
     with open(filename) as file:
         next(file, None) 
         rows = [line.strip().split(',') for line in file]   
-        for row in rows:             
-            total_cost = total_cost + int(row[1]) * float(row[2])
+        for row in rows:
+            try:             
+                total_cost = total_cost + int(row[1]) * float(row[2])
+            except ValueError:
+                print('Error in:', row)
         return total_cost
 
 cost = portfolio_cost('Data/portfolio.csv')
