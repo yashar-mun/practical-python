2b2406ae323d591efd0273b0bb239e1e0f552a28
Yashar Tavakoli <yt3165@mun.ca>
2021-05-22 14:03:35 -0230

    Exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,9 @@
 # pcost.py
-#
-# Exercise 1.27
+
+total_cost = 0.0
+with open("Data/portfolio.csv") as file:
+    next(file, None) 
+    rows = [line.strip().split(',') for line in file]   
+    for row in rows:             
+        total_cost = total_cost + int(row[1]) * float(row[2])
+print('Total cost:', total_cost)
