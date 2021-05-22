165c82d598a4eeaae9ee6fae09c2a7bbb7d7ffd8
Yashar Tavakoli <yt3165@mun.ca>
2021-05-22 10:30:48 -0230

    Exercise 1.8

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -5,8 +5,15 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 
-while principal > 0:
-    principal = principal * (1+rate/12) - payment
-    total_paid = total_paid + payment
+months = 0
 
-print('Total paid', total_paid)
+while principal > 0:
+    principal = principal * (1 + rate / 12) - payment
+    total_paid = total_paid + payment
+    months = months + 1
+
+    if months <= 12:
+        principal = principal - 1000.0
+        total_paid = total_paid + 1000.0
+
+print('Total paid', round(total_paid,2), 'in', months, 'months')
