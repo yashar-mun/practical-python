fd28c7509716a7d78ada76fb977a0bc008b62f61
Yashar Tavakoli <yt3165@mun.ca>
2021-05-22 09:54:10 -0230

    Exercise 1.5 (+ deleting a wrong file)

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+
+bounce_count = 1
+height = 100
+while bounce_count <= 10:
+    height = height * (3/5)
+    print(bounce_count, round(height, 4))
+    bounce_count = bounce_count + 1
