3909dd5167cff86fa2146c5e3c2af309a84cf1f9
Yashar Tavakoli <yt3165@mun.ca>
2021-05-25 19:30:06 -0230

    Exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,17 @@
 # report.py
-#
-# Exercise 2.4
+
+import csv
+
+def read_portfolio(filename):
+
+    portfolio = []
+
+    with open(filename) as file:
+        rows = csv.reader(file)
+        next(file, None)
+
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+
+    return portfolio
