c81497b054a3d13e19b455b9b95b90750ba7f537
Yashar Tavakoli <yt3165@mun.ca>
2021-05-26 12:26:37 -0230

    Exercise 2.6 (With a different style of file import, compared to Exercise 2.5.)

--- Work/report.py
+++ Work/report.py
@@ -15,3 +15,20 @@ def read_portfolio(filename):
             portfolio.append(item)
 
     return portfolio
+
+def read_prices(filename):
+
+    import csv
+
+    rows = csv.reader(open(filename, 'r'))
+
+    prices = {}
+    for row in rows:
+        try:
+            stock = row[0]
+            price = row[1]
+            prices[stock] = float(price)
+        except IndexError:
+            pass
+
+    return prices
