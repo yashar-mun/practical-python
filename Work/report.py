0fc330168e843a3c6e1546e70a57944c704c6cf6
Yashar Tavakoli <yt3165@mun.ca>
2021-05-26 15:31:47 -0230

    Exercise 2.12 (Using f-strings and list insert.)

--- Work/report.py
+++ Work/report.py
@@ -38,8 +38,8 @@ def make_report(portfolio, prices):
     report_rows = []
 
     for item in portfolio:
-        report_rows.append((item['name'],item['shares'],prices[item['name']],
-        prices[item['name']]-item['price']))
+        report_rows.append((item['name'],item['shares'],round(prices[item['name']],2),
+        round(prices[item['name']]-item['price'],2)))
 
     return report_rows
 
@@ -49,7 +49,12 @@ prices    = read_prices('Data/prices.csv')
 
 report = make_report(portfolio, prices)
 
-for name, shares, price, change in report:
-        print(f'{name:>10s} {shares:>10d} {price:>10.2f} {change:>10.2f}')
 
-
+report.insert(0, ('Name', 'Shares', 'Price', 'Change'))
+dash = '-' * 40
+for i in range(len(report)):
+    if i == 0:
+      print('{:>10s}{:>10s}{:>10s}{:>10s}'.format(report[i][0],report[i][1],report[i][2],report[i][3]))
+      print(dash)
+    else:
+      print('{:>10s}{:>10d}{:>10.2f}{:>10.2f}'.format(report[i][0],report[i][1],report[i][2],report[i][3]))
