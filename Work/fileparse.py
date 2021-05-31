e9f9a79cba6f2dd9554bb1611eb66efea78b3e11
Yashar Tavakoli <yt3165@mun.ca>
2021-05-30 21:52:33 -0230

    Exercise 3.4

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,25 +1,35 @@
 # fileparse.py
 
-from Work.report import print_report
 import csv
 
-def parse_csv(filename):
+def parse_csv(filename, select=None):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as f:
         rows = csv.reader(f)
 
-        # Read the file headers
+        
         headers = next(rows)
+
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
+
         records = []
         for row in rows:
-            if not row:    # Skip rows with no data
+            if not row:    
                 continue
+            
+            if indices:
+                row = [ row[index] for index in indices ]
+
             record = dict(zip(headers, row))
             records.append(record)
 
     return records
 
-portfolio = parse_csv('Data/portfolio.csv')
+portfolio = parse_csv('Data/portfolio.csv', select=['name','shares'])
 print(portfolio)
