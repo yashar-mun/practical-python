8bd6114dbdde2b2eec9a9db6c9a9fddf5ac8fdc8
Yashar Tavakoli <yt3165@mun.ca>
2021-05-31 11:57:00 -0230

    Exercise 3.17

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,42 +1,42 @@
 # fileparse.py
 import csv
 
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
+def parse_csv(line, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
  
     if select and not has_headers:
         raise RuntimeError('select requires column headers')
 
-    with open(filename) as f:
-        rows = csv.reader(f, delimiter=delimiter)
+    rows = csv.reader(line, delimiter=delimiter)
 
-        headers = next(rows) if has_headers else []
+
+    headers = next(rows) if has_headers else []
+
+    if select:
+        indices = [ headers.index(colname) for colname in select ]
+        headers = select
+
+    records = []
+    for rowno, row in enumerate(rows, 1):
+        if not row:    
+            continue
+
 
         if select:
-            indices = [ headers.index(colname) for colname in select ]
-            headers = select
+            row = [ row[index] for index in indices]
 
-        records = []
-        for rowno, row in enumerate(rows, 1):
-            if not row:    
+        if types:
+            try:
+                row = [func(val) for func, val in zip(types, row)]
+            except ValueError as e:
+                if not silence_errors:
+                    print(f"Row {rowno}: Couldn't convert {row}")
+                    print(f"Row {rowno}: Reason {e}")
                 continue
 
+        if headers:
+            record = dict(zip(headers, row))
+        else:
+            record = tuple(row)
+        records.append(record)
 
-            if select:
-                row = [ row[index] for index in indices]
-
-            if types:
-                try:
-                    row = [func(val) for func, val in zip(types, row)]
-                except ValueError as e:
-                    if not silence_errors:
-                        print(f"Row {rowno}: Couldn't convert {row}")
-                        print(f"Row {rowno}: Reason {e}")
-                    continue
-
-            if headers:
-                record = dict(zip(headers, row))
-            else:
-                record = tuple(row)
-            records.append(record)
-
-        return records
+    return records
